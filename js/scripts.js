d8066198fc502523cbee082e8b24425e65d8418a
João <joaovictor2126@gmail.com>
2026-01-28 19:58:10 -0300

    feat: adiciona arquivos HTML, CSS e JS base do projeto

---
+++ js/scripts.js
@@ -0,0 +1,132 @@
+"use strict";
+
+const addTaskForm = document.querySelector("#add-task-form");
+const addTaskInput = document.querySelector("#add-task-input");
+const taskList = document.querySelector("#task-list");
+const taskListSearchbar = document.querySelector(".task-list-searchbar");
+
+// Aqui criamos uma função para verificar se existem tarefas cadastradas
+function verificarTarefas() {
+    const tarefas = document.querySelectorAll(".task-container");
+
+    // caso tenha 1 ou mais tarefas, retorna 1. Do contrário retorna 0
+    if (tarefas.length >= 1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+addTaskForm.addEventListener("submit", (evento) => {
+    // prevenindo a pagina de dar reload ao clicar no botao tipo submit
+    evento.preventDefault();
+
+    // o trim remove espaços antes e depois do texto
+    // também evita textos vazios
+    const taskText = addTaskInput.value.trim();
+
+    if (taskText === "") return;
+
+    // removendo o informativo de "suas tarefas aparecerão aqui..."
+    const taskListInfo = document.querySelector(".task-list-info");
+    taskListInfo.classList.add("hidden");
+
+    // criando a estrutura da tarefa
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
+    const taskTextElement = document.createElement("li");
+    taskTextElement.textContent = taskText;
+
+    const taskBtnContainer = document.createElement("div");
+    taskBtnContainer.classList.add("task-btn-container");
+
+    // criando os botões concluir, editar e remover tarefa
+    const completeTaskBtn = document.createElement("button");
+    completeTaskBtn.type = "button";
+    completeTaskBtn.classList.add("complete-task-btn");
+    
+    const editTaskBtn = document.createElement("button");
+    editTaskBtn.type = "button";
+    editTaskBtn.classList.add("edit-task-btn");
+    editTaskBtn.innerHTML = `<i class="fa-solid fa-pen"></i>`;
+    
+    const deleteTaskBtn = document.createElement("button");
+    deleteTaskBtn.type = "button";
+    deleteTaskBtn.classList.add("delete-task-btn");
+    deleteTaskBtn.innerHTML = `<i class="fa-solid fa-x"></i>`;
+
+    // organizando a hierarquia da estrutura da tarefa
+    taskBtnContainer.append(editTaskBtn, deleteTaskBtn);
+    taskContainer.append(completeTaskBtn, taskTextElement, taskBtnContainer);
+    taskList.appendChild(taskContainer);
+
+    addTaskInput.value = "";
+})
+
+taskList.addEventListener("click", (evento) => {
+    // COMPLETE
+    const completeBtn = evento.target.closest(".complete-task-btn");
+
+    if (completeBtn) {
+        const taskContainer = completeBtn.closest(".task-container");
+        const taskTextElement = taskContainer.querySelector("li");
+
+        taskTextElement.classList.add("finished-task-text");
+
+        const editBtn = taskContainer.querySelector(".edit-task-btn");
+        editBtn.remove();
+    }
+
+    // DELETE
+    const deleteBtn = evento.target.closest(".delete-task-btn");
+
+    if (deleteBtn) {
+        const taskContainer = deleteBtn.closest(".task-container");
+        taskContainer.remove();
+        
+        if (verificarTarefas() === 0) {
+            const taskListInfo = document.querySelector(".task-list-info");
+            taskListInfo.classList.remove("hidden");
+        }
+        
+        return;
+    }
+
+    // EDIT
+    const editBtn = evento.target.closest(".edit-task-btn");
+
+    if (!editBtn) {
+        return;
+    }
+
+    const taskContainer = editBtn.closest(".task-container");
+    const taskTextElement = taskContainer.querySelector("li");
+
+    taskTextElement.setAttribute("contenteditable", "true");
+    taskTextElement.focus();
+
+    taskTextElement.addEventListener("blur", () => {
+        taskTextElement.removeAttribute("contenteditable");
+
+        if (taskTextElement.textContent.trim() === "") {
+            taskTextElement.textContent = "Tarefa sem nome";
+        }
+    }, { once: true });
+})
+
+taskListSearchbar.addEventListener("keyup", () => {
+    const searchbarTxt = taskListSearchbar.value.toLowerCase().trim();
+    const tasks = taskList.querySelectorAll("li");
+
+    tasks.forEach(task => {
+        const taskContainer = task.closest(".task-container");
+
+        if (task.textContent.toLowerCase().includes(searchbarTxt)) {
+            taskContainer.classList.remove("hidden");
+        } else {
+            taskContainer.classList.add("hidden");
+        }
+    })
+})
